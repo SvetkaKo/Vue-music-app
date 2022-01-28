57b33d0c363058ebca4815daac633587046e9222
SvetkaKo <skovakina@gmail.com>
2022-01-28 22:25:36 +0300

    init

---
+++ src/store/index.js
@@ -0,0 +1,60 @@
+import { createStore } from 'vuex';
+import { auth, usersCollection } from '@/includes/firebase';
+
+export default createStore({
+  state: {
+    authModalShow: false,
+    userLoggedIn: false,
+  },
+  mutations: {
+    // login / Register button
+    toggleAuthModal(state) {
+      state.authModalShow = !state.authModalShow;
+    },
+    // I don't need this 'getters' since I can use mapState from vuex
+    // getters: {
+    //   authModalShow(state) {
+    //     return state.authModalShow;
+    //   },
+    // },
+    toggleAuth(state) {
+      state.userLoggedIn = !state.userLoggedIn;
+    },
+  },
+  actions: {
+    async register({ commit }, payload) {
+      const userCred = await auth.createUserWithEmailAndPassword(payload.email, payload.password);
+
+      await usersCollection.doc(userCred.user.uid).set({
+        name: payload.name,
+        email: payload.email,
+        age: payload.age,
+        country: payload.country,
+      });
+
+      await userCred.user.updateProfile({
+        displayName: payload.name,
+      });
+
+      commit('toggleAuth');
+    },
+
+    async login({ commit }, payload) {
+      await auth.signInWithEmailAndPassword(payload.email, payload.password);
+
+      commit('toggleAuth');
+    },
+    init_login({ commit }) {
+      const user = auth.currentUser;
+
+      if (user) {
+        commit('toggleAuth');
+      }
+    },
+    async signout({ commit }) {
+      await auth.signOut();
+
+      commit('toggleAuth');
+    },
+  },
+});
