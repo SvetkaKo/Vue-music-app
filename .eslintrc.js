4fe4692c6e168a2b67099e66a2caf1ec0ae234ce
SvetkaKo <skovakina@gmail.com>
2022-01-29 18:06:53 +0300

    manage router

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'linebreak-style': 0,
-    'max-len': ['error', { code: 200 }],
+    'max-len': ['error', { code: 300 }],
   },
   overrides: [
     {
--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,63 @@
 import { createRouter, createWebHistory } from 'vue-router';
-// import Home from '../views/Home.vue';
+import Home from '@/views/Home.vue';
+import About from '@/views/About.vue';
+import Manage from '@/views/Manage.vue';
+import store from '@/store';
 
 const routes = [
   {
-    // path: '/',
-    // name: 'Home',
-    // component: Home,
+    name: 'home',
+    path: '/',
+    component: Home,
   },
   {
-    // path: '/about',
-    // name: 'About',
-    // // route level code-splitting
-    // // this generates a separate chunk (about.[hash].js) for this route
-    // // which is lazy-loaded when the route is visited.
-    // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    name: 'about',
+    path: '/about',
+    component: About,
+  },
+  {
+    name: 'manage',
+    // alias: '/manage',
+    path: '/manage-music',
+    meta: {
+      requiresAuth: true,
+    },
+    component: Manage,
+    beforeEnter: (to, from, next) => {
+      // console.log('manage routr guard');
+      next();
+    },
+  },
+  // redirect user from the old path to the new path
+  {
+    path: '/manage',
+    redirect: { name: 'manage' },
+  },
+
+  // redirect user from the non-existent path to the home page
+  {
+    path: '/:catchAll(.*)*',
+    redirect: { name: 'home' },
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+  linkExactActiveClass: 'text-yellow-500',
+});
+
+router.beforeEach((to, from, next) => {
+  if (!to.matched.some((record) => record.meta.requiresAuth)) {
+    next();
+    return;
+  }
+
+  if (store.state.userLoggedIn) {
+    next();
+  } else {
+    next({ name: 'home' });
+  }
 });
 
 export default router;
