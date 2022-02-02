29ad47180d866681d18257656bac71cdad0d5494
SvetkaKo <skovakina@gmail.com>
2022-02-02 21:35:44 +0300

    added uploading

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,23 @@
 module.exports = {
   root: true,
+
   env: {
     node: true,
   },
+
   extends: ['plugin:vue/vue3-essential', '@vue/airbnb'],
+
   parserOptions: {
     parser: 'babel-eslint',
   },
+
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-console': 'off',
+    'no-debugger': 'off',
     'linebreak-style': 0,
     'max-len': ['error', { code: 300 }],
   },
+
   overrides: [
     {
       files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
@@ -21,4 +26,6 @@ module.exports = {
       },
     },
   ],
+
+  extends: ['plugin:vue/vue3-essential', '@vue/airbnb'],
 };
