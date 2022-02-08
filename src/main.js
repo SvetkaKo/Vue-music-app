0891e9907af862783ca1486e9a199aff0d9f297e
SvetkaKo <skovakina@gmail.com>
2022-02-08 12:24:42 +0300

    added icon directive

--- src/main.js
+++ src/main.js
@@ -3,9 +3,10 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import VeeValidatePlugin from './includes/validation';
+import { auth } from './includes/firebase';
+import Icon from './directives/icon';
 import './assets/tailwind.css';
 import './assets/main.css';
-import { auth } from './includes/firebase';
 
 let app;
 
@@ -15,6 +16,7 @@ auth.onAuthStateChanged(() => {
     app.use(store);
     app.use(router);
     app.use(VeeValidatePlugin);
+    app.directive('icon', Icon);
 
     app.mount('#app');
   }
